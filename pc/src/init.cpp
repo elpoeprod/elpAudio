3e5ea4887eab1f08f3b27b4c8bcacf4a6120ab7d
elpoeprod <elpoepstudios@gmail.com>
2025-08-25 14:49:14 +0300

    fonts+more tabs

--- pc/src/init.cpp
+++ pc/src/init.cpp
@@ -6,6 +6,7 @@
                             a->event_draw=a##_Draw;
 
 GBRoom *rmPlayerMain;
+GBFont *fntMain;
 
 void eaInit() {
     initSettings();
@@ -14,14 +15,20 @@ void eaInit() {
                        (GB_CamSetup){0,0,eaSettings->w,eaSettings->h,0},
                        (GB_CamSetup){0,0,eaSettings->w,eaSettings->h,0},
                        -1,(GB_CamTarget){0,0,0,0});
+    window::set_size(eaSettings->w,eaSettings->h);
+
+    fntMain=font::add("themes/sourcesans.ttf",12);
+
+    draw::set_font(fntMain);
+
     room::current(rmPlayerMain);
     initButton();
-    InitObject(btPrev,  0,  16)
-    InitObject(btStop,  32, 16)
-    InitObject(btPlay,  64, 16)
-    InitObject(btPause, 96, 16)
-    InitObject(btNext,  128,16)
-
+    InitObject(btPrev,      0,  16)
+    InitObject(btStop,      32, 16)
+    InitObject(btPlay,      64, 16)
+    InitObject(btPause,     96, 16)
+    InitObject(btNext,      128,16)
+    InitObject(btAddMusic,  288,32)
     initPlaylist();
 
 }
