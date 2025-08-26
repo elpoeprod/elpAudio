b4e39b1eb9cc1a456e8adb72a307ec14854b6e95
elpoeprod <elpoepstudios@gmail.com>
2025-08-26 23:17:57 +0300

    more buttons

--- pc/src/buttons.cpp
+++ pc/src/buttons.cpp
@@ -8,16 +8,22 @@ button(btPause)
 button(btNext)
 button(btPrev)
 button(btAddMusic)
+button(objFloatText)
+button(objVisualiser)
+button(btChangeVis)
 
 #undef button
 
 
 void initButton() {
-    btPlay=     object::add(eaTheme->bt[BUTTON_PLAY].spr   ,nullptr);
-    btStop=     object::add(eaTheme->bt[BUTTON_STOP].spr   ,nullptr);
-    btPause=    object::add(eaTheme->bt[BUTTON_PAUSE].spr  ,nullptr);
-    btPrev=     object::add(eaTheme->bt[BUTTON_PREV].spr   ,nullptr);
-    btNext=     object::add(eaTheme->bt[BUTTON_NEXT].spr   ,nullptr);
-    btAddMusic= object::add(eaTheme->bt[BUTTON_ADDMUS].spr ,nullptr);
+    btPlay=			object::add(eaTheme->bt[BUTTON_PLAY].spr		,nullptr);
+    btStop=			object::add(eaTheme->bt[BUTTON_STOP].spr		,nullptr);
+    btPause=		object::add(eaTheme->bt[BUTTON_PAUSE].spr		,nullptr);
+    btPrev=			object::add(eaTheme->bt[BUTTON_PREV].spr		,nullptr);
+    btNext=			object::add(eaTheme->bt[BUTTON_NEXT].spr		,nullptr);
+    btAddMusic=		object::add(eaTheme->bt[BUTTON_ADDMUS].spr		,nullptr);
+	objFloatText=	object::add(eaTheme->bt[FLOAT_BACKGROUND].spr	,eaTheme->bt[FLOAT_FOREGROUND].spr);
+	objVisualiser=	object::add(eaTheme->bt[VISUALISER_BG].spr		,eaTheme->bt[VISUALISER_FG].spr);
+	btChangeVis=	object::add(eaTheme->bt[BUTTON_CHANGE_VIS].spr	,nullptr);
     return;
 }
